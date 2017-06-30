685bd5e005ce1499016dad8277b7dc7573139d0a
jerusalemc <528122401@qq.com>
2017-06-30 10:33:31 +0800

    remove useless file

--- start_client.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import time
-
-for idx in range(1, 3):
-    result = subprocess.Popen(["./client.py", "--cid", str(idx)])
-    out, err = result.communicate()
-    # time.sleep(3)
-
-
